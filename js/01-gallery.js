eb530cb4bb45652f5179187e40632e5e93dd9c7a
Orest-M <orestmikhal@gmail.com>
2022-12-19 15:33:38 +0200

    1

---
+++ js/01-gallery.js
@@ -0,0 +1,35 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    (galleryItem) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${galleryItem.original}">
+            <img 
+                class="gallery__image"
+                src="${galleryItem.preview}" 
+                data-source="${galleryItem.original}" 
+                alt="${galleryItem.description}"  
+            >
+        </a>
+    </div>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', markup);
+
+// console.log(markup);
+
+gallery.addEventListener('click', onClick);
+
+function onClick(evt) {
+  // console.log(evt.target);
+
+  if (evt.target.classList.contains('gallery__image')) {
+    const currentImg = evt.target.closest('.gallery__img');
+    console.log(currentImg);
+  }
+}
