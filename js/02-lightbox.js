593ec4cb74b644a0899b1069201bf02040b2ff19
Orest-M <orestmikhal@gmail.com>
2022-12-20 13:57:45 +0200

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -14,8 +14,18 @@ const markup = galleryItems
 
 gallery.insertAdjacentHTML('afterbegin', markup);
 
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+});
+
 gallery.addEventListener('click', onClick);
 
-function onClick(evt) {}
+function onClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+}
 
 // console.log(galleryItems);
